7e8f5affdd915b39bf4111cedb35a44c22f2313f
dokkyunYU <ydgar001@naver.com>
2023-01-26 22:49:32 +0900

    Design : Add KioskSelect.jsx + KioskSelect.scss

--- DGRR/frontend/DGRR-App/src/App.jsx
+++ DGRR/frontend/DGRR-App/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from './components/user/Login'
+import KioskSelect from './components/kiosk/KioskSelect'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/KioskSelect" element={<KioskSelect />} />
     </Routes>
   )
 }
---
+++ DGRR/frontend/DGRR-App/src/components/kiosk/KioskSelect.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../../scss/KioskSelect.scss"
+
+const KioskSelect = () => {
+    return (
+      <div className="KioskBackground">
+        <div className="TitleBox">
+          <div className="TitleText"><p className="RealText">DG.RR</p></div>
+          <hr className='Pin'></hr>
+        </div>
+      </div>
+    )
+}
+
+export default KioskSelect
