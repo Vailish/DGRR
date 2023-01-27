f7cb8f17863a5386a9fe937d86db6da239525521
김지환 <wlghks960@naver.com>
2023-01-27 17:51:54 +0900

    Design : Add LoginComponent

--- DGRR/frontend/DGRR-App/src/App.jsx
+++ DGRR/frontend/DGRR-App/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Login from './components/user/Login'
-
+import IdFind from './components/user/Find/IdFind'
+import PwFind from './components/user/Find/PwFind'
+import Login from './components/user/Login/Login'
+import Join from './components/user/Join/Join'
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/join" element={<Join />} />
+      <Route path="/findId" element={<IdFind />} />
+      <Route path="/findPw" element={<PwFind />} />
     </Routes>
   )
 }
---
+++ DGRR/frontend/DGRR-App/src/components/user/Find/IdFind.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IdFind = () => {
+  return (
+    <div>
+      <h1>아이디 찾기 페이지 입니다.</h1>
+    </div>
+  )
+}
+
+export default IdFind
