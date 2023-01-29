8ad2724879e4fb2505e80e04e3b0a116b1c957ca
dokkyunYU <ydgar001@naver.com>
2023-01-30 02:19:46 +0900

    Remove : Delete Test.jsx

--- DGRR/frontend/DGRR-App/src/App.jsx
+++ DGRR/frontend/DGRR-App/src/App.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from './components/user/Login'
 import KioskSelect from './components/kiosk/KioskSelect'
-import TEST from './components/kiosk/TEST'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/KioskSelect" element={<KioskSelect />} />
-      <Route path="/TEST" element={<TEST/>} />
     </Routes>
   )
 }
