e1e1e9c3ca2de4eb386e79f46755cc268132d6a8
JeongBeomi <qja367@gmail.com>
2023-02-01 17:34:44 +0900

    Design : Add mainpage.scss

--- DGRR/frontend/DGRR-App/src/App.jsx
+++ DGRR/frontend/DGRR-App/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import LoginForm from './containers/user/LoginForm'
 import RegisterForm from './containers/user/RegisterForm'
 import { Routes, Route } from 'react-router-dom'
-import MainPage from './pages/MainPage'
+import MainPage from './pages/Mainpage'
+import Rankingpage from './pages/Rankingpage'
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <Route path="/" element={<LoginForm />} />
       <Route path="/register" element={<RegisterForm />} />
       <Route path="/main" element={<MainPage />} />
-      <Route path="/mainpage" element={<MainPage />} />
+      <Route path=":nickName" element={<MainPage />} />
+      <Route path="ranking" element={<Rankingpage />} />
     </Routes>
   )
 }
