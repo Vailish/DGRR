167609123b49d1706f91d5c78b869dbbd09db0c0
YuHyeonGeun-KOR <YuHyeonGuen.dev@gmail.com>
2023-02-01 14:22:09 +0900

    Docs : ADD New Server

---
+++ DGRR/webRTC-1/src/main/java/com/ssafy/b102/WebRtc1Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.b102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRtc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRtc1Application.class, args);
+	}
+
+}
