00a4781a1bff579cf269ca8bf95bfb3f2439ea60
dokkyunYU <ydgar001@naver.com>
2023-02-09 23:20:19 +0900

    Feat : Update 오프라인 게임 결과 차트

--- DGRR/frontend/DGRR-App/src/components/kiosk/offline/KioskOfflineResult.jsx
+++ DGRR/frontend/DGRR-App/src/components/kiosk/offline/KioskOfflineResult.jsx
@@ -11,39 +11,27 @@ import ResultChart from './ResultChart'
 import OfflineResultChart from './OfflineResultChart'
 import playerScore from '../../../scss/_variables.scss'
 
-const ShowImage = props => {
-  const { x, y, width, height, value } = props
-  const radius = 10
-
-  return (
-    <g>
-      <image href="../../../public/logo192.png" width="10%" height="10%" x="10vw" y="10vh" />
-    </g>
-  )
-}
-
-const data = [
-  { name: '', pv: 298 },
-  //   { name: '김볼링', pv: 99 },
-]
-
 const KioskOfflineResult = () => {
-  const dispatch = useDispatch()
-
-  const onSendAllScore = () => dispatch(sendAllScore())
-  const ChangeLength = () => {
-    playerScore = { ...playerScore, playerScoreLength: '20%' }
-  }
-  const resultData = useSelector(state => state.OfflineLoginUsers.gamingPlayers)
-  const playersList = Object.keys(resultData)
+  const playerList = useSelector(state => Object.keys(state.OfflineLoginUsers.gamingPlayers))
+  console.log(playerList)
+  const gamingPlayers = useSelector(state => state.OfflineLoginUsers.gamingPlayers)
 
   const EachPlayerBlock = props => {
-    const { data } = props
+    const { scoreSumList, playerNum } = props
+    const lastScore = scoreSumList[scoreSumList.length - 1]
+    const widthRatio = (lastScore / 300) * 95
+
     return (
       <div className="EachPlayerBlock">
-        <div className="PlayerProfile"></div>
-        <div className="PlayerScoreChart">
-          <OfflineResultChart data={data} width="100%" height="100%" />
+        <div className="ResultChartProfileBlock">
+          <div className="ResultChartProfile">{playerNum}</div>
+        </div>
+        <div className="PlayerScoreChartBlock">
+          <div className="PlayerScoreChart" style={{ width: `${widthRatio}%` }}>
+            <div className="PlayerScoreChartBar">{lastScore}</div>
+          </div>
+          {/* <div className="PlayerScoreChart" style={{ transform: `scaleX(${widthRatio})` }}></div> */}
+          {/* <div className="PlayerScoreNumber">{lastScore}</div> */}
         </div>
       </div>
     )
@@ -51,30 +39,68 @@ const KioskOfflineResult = () => {
 
   return (
     <div className="KioskBackground">
-      <KioskNavBlock goFrontTo="/KioskLogin" />
+      <KioskNavBlock goBackTo={'/KioskOfflineGame'} goFrontTo={'/KioskLogin'} />
       <div className="ResultContentBlock">
-        {playersList.map((player, index) => {
-          const gameBoardResult = resultData[player].gameBoardResult
-          const pv = gameBoardResult[gameBoardResult.length - 1]
-          const data = { name: '', pv }
-          return <EachPlayerBlock data={data} width="100%" height="100%" />
-        })}
-        {/* <EachPlayerBlock/>
-        <div className="EachPlayerBlock">
-          <div className="PlayerProfile"></div>
-          <div className="PlayerScoreChart">
-            <OfflineResultChart data={data} width="100%" height="100%" />
-          </div>
+        <div className="ResultChartBlock">
+          {playerList.map(player => {
+            return <EachPlayerBlock scoreSumList={gamingPlayers[player].gameBoardResult} playerNum={player} />
+          })}
         </div>
-        <div className="EachPlayerBlock">
-          <div className="PlayerProfile"></div>
-          <div className="PlayerScoreChart">
-            <OfflineResultChart data={data} width="100%" height="100%" />
-          </div>
-        </div> */}
       </div>
     </div>
   )
+
+  // const dispatch = useDispatch()
+
+  // const onSendAllScore = () => dispatch(sendAllScore())
+  // const ChangeLength = () => {
+  //   playerScore = { ...playerScore, playerScoreLength: '20%' }
+  // }
+  // const resultData = useSelector(state => state.OfflineLoginUsers.gamingPlayers)
+  // const playersList = Object.keys(resultData)
+
+  // const EachPlayerBlock = props => {
+  //   const { data } = props
+  //   return (
+  //     <div className="EachPlayerBlock">
+  //       <div className="PlayerProfile"></div>
+  //       <div className="PlayerScoreChart">
+  //         <OfflineResultChart data={data} width="100%" height="100%" />
+  //       </div>
+  //     </div>
+  //   )
+  // }
+
+  // return (
+  //   <div className="KioskBackground">
+  //     <KioskNavBlock goFrontTo="/KioskLogin" />
+  //     <div className="ResultContentBlock">
+  //       {playersList.map((player, index) => {
+  //         console.log(player)
+  //         const gameBoardResult = resultData[player].gameBoardResult
+  //         console.log(gameBoardResult)
+  //         const pv = gameBoardResult[gameBoardResult.length - 1]
+  //         console.log(pv)
+  //         const data = [{ name: player, pv }]
+  //         // return <EachPlayerBlock data={data} width="100%" height="100%" />
+  //         return <div style={{ width: `${pv}vw`, background: 'white', height: '5vh' }}></div>
+  //       })}
+  //       {/* <EachPlayerBlock/>
+  //       <div className="EachPlayerBlock">
+  //         <div className="PlayerProfile"></div>
+  //         <div className="PlayerScoreChart">
+  //           <OfflineResultChart data={data} width="100%" height="100%" />
+  //         </div>
+  //       </div>
+  //       <div className="EachPlayerBlock">
+  //         <div className="PlayerProfile"></div>
+  //         <div className="PlayerScoreChart">
+  //           <OfflineResultChart data={data} width="100%" height="100%" />
+  //         </div>
+  //       </div> */}
+  //     </div>
+  //   </div>
+  // )
 }
 
 export default KioskOfflineResult
