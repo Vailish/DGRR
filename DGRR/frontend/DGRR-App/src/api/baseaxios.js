627a68257a170dfd2a9fe26cf1ad2b4931e7e270
JeongBeomi <qja367@gmail.com>
2023-02-11 23:07:21 +0900

    Feat : Add searchbar

--- DGRR/frontend/DGRR-App/src/api/baseaxios.js
+++ DGRR/frontend/DGRR-App/src/api/baseaxios.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const baseaxios = axios.create({
-  baseURL: 'http://192.168.31.142:8080',
+  baseURL: 'http://172.30.1.50:8080',
+  headers: {
+    'Content-Type': 'application/json; charset=UTF-8'
+  }
   // headers: {
   //   'Authorization': 'Bearer ' + token,
   // },
