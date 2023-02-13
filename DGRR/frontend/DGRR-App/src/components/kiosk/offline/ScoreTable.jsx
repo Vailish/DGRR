cbe5ae3bbde11faabd7fdf6d0bec1280bfd33bab
dokkyunYU <ydgar001@naver.com>
2023-02-13 15:26:29 +0900

    Fix : Update ScoreTable 내용에서 안쓰는 것들 제거

--- DGRR/frontend/DGRR-App/src/components/kiosk/offline/ScoreTable.jsx
+++ DGRR/frontend/DGRR-App/src/components/kiosk/offline/ScoreTable.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import '../../../scss/ScoreTable.scss'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-// import KioskLoginPlayer from './KioskLoginPlayer'
-import { addPlayer } from '../../../store/OfflineLoginUsers'
-// import axios from 'axios'
-import { apis } from '../../../API/api'
 import ScoreTableBlock from './ScoreTableBlock'
 
 const ScoreTable = props => {
   const NumArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   const { playerNum, isInput, scoreSumArray, scoreArray } = props
 
-  console.log(playerNum)
   return (
     <div className="ScoreTable">
       {NumArray.map((num, index) => {
