57cf8814c9a2fc318353de96c7e2172d2aa740ea
JeongBeomi <qja367@gmail.com>
2023-02-14 17:04:11 +0900

    Feat : Update axios url

--- DGRR/frontend/DGRR-App/src/API/api.jsx
+++ DGRR/frontend/DGRR-App/src/API/api.jsx
@@ -2,8 +2,7 @@ import axios from 'axios'
 
 export const api = axios.create({
   // 이 부분 수정
-  // baseURL: 'http://3.38.97.157:8080/api/',
-  baseURL: 'http://192.168.31.142:8080/api/',
+  baseURL: 'http://3.38.97.157:8080/api/',
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
     accept: 'application/json,',
--- DGRR/frontend/DGRR-App/src/API/request.js
+++ DGRR/frontend/DGRR-App/src/API/request.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 export const request = axios.create({
-  // baseURL: "http://3.38.97.157:8080",
-  baseURL: "http://192.168.31.142:8080",
+  baseURL: "http://3.38.97.157:8080",
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
     accept: 'application/json,',
