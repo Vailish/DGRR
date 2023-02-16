407cf69af9a60c1db461aa65953523502897770d
JeongBeomi <qja367@gmail.com>
2023-02-16 14:59:59 +0900

    Update : maincode

--- DGRR/frontend/DGRR-App/src/api/baseaxios.js
+++ DGRR/frontend/DGRR-App/src/api/baseaxios.js
@@ -1,14 +1,10 @@
 import axios from 'axios'
 
 const baseaxios = axios.create({
-  // baseURL: 'https://i8b102.p.ssafy.io/',
-  baseURL: 'http://192.168.31.142:8080/',
+  baseURL: 'https://i8b102.p.ssafy.io/',
   headers: {
     'Content-Type': 'application/json; charset=UTF-8',
   },
-  // headers: {
-  //   'Authorization': 'Bearer ' + token,
-  // },
 })
 
 export default baseaxios
